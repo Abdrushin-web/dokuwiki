688ffe6cb236467e201ff3a409537fa550278768
Marek Istvanek <marek.istvanek@mycronic.com>
2024-01-30 23:24:10 +0100

    epub progress

--- conf/userscript.js
+++ conf/userscript.js
@@ -1,26 +1,26 @@
 var breadcrumbs, sticky, stickyYOffset, trace;
 
-window.addEventListener(
-    'DOMContentLoaded',
-    (event) =>
-    {
-        breadcrumbs = document.getElementsByClassName("breadcrumbs")[0];
-        sticky = breadcrumbs.getElementsByClassName("youarehere")[0];
-        trace = breadcrumbs.getElementsByClassName("trace")[0];
-        stickyYOffset = sticky.offsetTop;
-    });
+// window.addEventListener(
+//     'DOMContentLoaded',
+//     (event) =>
+//     {
+//         breadcrumbs = document.getElementsByClassName("breadcrumbs")[0];
+//         sticky = breadcrumbs.getElementsByClassName("youarehere")[0];
+//         trace = breadcrumbs.getElementsByClassName("trace")[0];
+//         stickyYOffset = sticky.offsetTop;
+//     });
 
-window.onscroll = () =>
-{
-    if (breadcrumbs === undefined)
-        return;
-    const stickyClass = "sticky";
-    var classes = sticky.classList;
-    if (window.pageYOffset >= stickyYOffset)
-        classes.add(stickyClass);
-    else
-        classes.remove(stickyClass);
-};
+// window.onscroll = () =>
+// {
+//     if (breadcrumbs === undefined)
+//         return;
+//     const stickyClass = "sticky";
+//     var classes = sticky.classList;
+//     if (window.pageYOffset >= stickyYOffset)
+//         classes.add(stickyClass);
+//     else
+//         classes.remove(stickyClass);
+// };
 
 
 /** Show each section link when hovering over each respective headline.
